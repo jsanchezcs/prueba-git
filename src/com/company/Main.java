21de24c2d53359cbd7643aa3a395640ce2a928ec
Jose Sanchez Moreno <jsanchezcs@gmail.com>
2018-08-30 16:15:06 +0200

    Nueva funcionalidad

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Esto es una nueva rama");
+        System.out.println("Nueva funcionalidad");
         System.out.println("Hola mundo!");
     }
 }
